28b8ca58fd4fe098191170f0758d51a1e76eaae5
yogaekapratama29 <yogaex29@gmail.com>
2024-06-10 22:06:42 +0700

    third

--- lib/pages/Main_Features/CalculatorGizi.dart
+++ lib/pages/Main_Features/CalculatorGizi.dart
@@ -28,8 +28,7 @@ class _CalculatorgiziState extends State<Calculatorgizi> {
           backgroundColor: Color.fromARGB(248, 255, 255, 255),
           color: Color.fromARGB(248, 244, 114, 181),
           animationDuration: Duration(milliseconds: 250),
-          onTap: (index) {
-          },
+          onTap: (index) {},
           items: [
             InkWell(
               onTap: () {
@@ -88,6 +87,253 @@ class _CalculatorgiziState extends State<Calculatorgizi> {
             )
           ]),
       appBar: AppBar(),
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20, top: 20),
+                    child: Text(
+                      "Informasi Nilai Gizi",
+                      style: GoogleFonts.poppins(
+                          fontSize: 28, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 360,
+                    height: 50,
+                    margin: EdgeInsets.only(left: 20),
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(248, 244, 114, 181),
+                        borderRadius: BorderRadius.circular(8)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          labelStyle: TextStyle(
+                            color: Color.fromARGB(185, 255, 255, 255),
+                          ),
+                          labelText: "Mau Cari Informasi Makanan Apa?",
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                color: Color.fromARGB(78, 255, 255, 255),
+                                width: 1),
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  // Section 1
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 165,
+                        height: 143,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/nasi.webp"),
+                                fit: BoxFit.cover),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 165,
+                              height: 35,
+                              color: Color.fromARGB(248, 244, 114, 181),
+                              child: Container(padding: EdgeInsets.only(left: 10,top: 5),
+                                  child: Text(
+                                "Nasi",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              )),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        width: 165,
+                        height: 143,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/asi.png"),
+                                fit: BoxFit.cover),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 165,
+                              height: 35,
+                              color: Color.fromARGB(248, 244, 114, 181),
+                              child: Container(padding: EdgeInsets.only(left: 10,top: 5),
+                                  child: Text(
+                                "Asi",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              )),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  // Section 2
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                     Container(
+                        width: 165,
+                        height: 143,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/bubur_instan.png"),
+                                fit: BoxFit.cover),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 165,
+                              height: 35,
+                              color: Color.fromARGB(248, 244, 114, 181),
+                              child: Container(padding: EdgeInsets.only(left: 10,top: 5),
+                                  child: Text(
+                                "Bubur Instan",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              )),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        width: 165,
+                        height: 143,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/bubur.png"),
+                                fit: BoxFit.cover),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 165,
+                              height: 35,
+                              color: Color.fromARGB(248, 244, 114, 181),
+                              child: Container(padding: EdgeInsets.only(left: 10,top: 5),
+                                  child: Text(
+                                "Bubur Beras",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              )),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  // Section 3
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 165,
+                        height: 143,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/biskuit.png"),
+                                fit: BoxFit.cover),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 165,
+                              height: 35,
+                              color: Color.fromARGB(248, 244, 114, 181),
+                              child: Container(padding: EdgeInsets.only(left: 10,top: 5),
+                                  child: Text(
+                                "Biskuit",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              )),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        width: 165,
+                        height: 143,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("images/cemilan.png"),
+                                fit: BoxFit.cover),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Container(
+                              width: 165,
+                              height: 35,
+                              color: Color.fromARGB(248, 244, 114, 181),
+                              child: Container(padding: EdgeInsets.only(left: 10,top: 5),
+                                  child: Text(
+                                "Cemilan",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              )),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/Main_Features/FormPendaftaran.dart
+++ lib/pages/Main_Features/FormPendaftaran.dart
@@ -12,7 +12,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-
 class Formpendaftaran extends StatefulWidget {
   const Formpendaftaran({super.key});
 
@@ -23,7 +22,8 @@ class Formpendaftaran extends StatefulWidget {
 class _FormpendaftaranState extends State<Formpendaftaran> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( bottomNavigationBar: CurvedNavigationBar(
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
           height: 75.0,
           backgroundColor: Color.fromARGB(248, 255, 255, 255),
           color: Color.fromARGB(248, 244, 114, 181),
@@ -87,6 +87,224 @@ class _FormpendaftaranState extends State<Formpendaftaran> {
                 size: 40,
               ),
             )
-          ]),appBar: AppBar(),);
+          ]),
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  "Form Daftar",
+                  style: GoogleFonts.poppins(
+                      fontSize: 28, fontWeight: FontWeight.w600),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: const Color.fromARGB(186, 158, 158, 158),
+                      ),
+                      labelText: "Masukan Alamat",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Masukan Kecamatan",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Masukan Kabupaten",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Pilih Posyandu",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Pilih Nama Anak",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "NIK",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Nama Anak",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Tempat,Tanggal Lahir",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 360,
+                height: 50,
+                margin: EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                          color: const Color.fromARGB(186, 158, 158, 158)),
+                      labelText: "Nama Ibu",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(78, 178, 174, 174),
+                            width: 2),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                  child: Container(
+                height: 50,
+                width: 360,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                        color: const Color.fromARGB(78, 178, 174, 174)),
+                    color: Color.fromARGB(248, 244, 114, 181)),
+                child: Center(
+                    child: Text(
+                  "Daftar",
+                  style: GoogleFonts.poppins(fontSize: 20, color: Colors.white),
+                )),
+              ))
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
